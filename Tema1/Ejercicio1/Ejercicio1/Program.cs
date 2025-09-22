a448b92064bd3ee0541294155da27802ad928053
CarlosOj22 <carlosojeda1336@gmail.com>
2025-09-22 11:52:32 +0200

    22/09/2025-ActualizacionTema1

---
+++ Tema1/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Ejercicio1
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int numero=1;
+            int numeroTotal = 0;
+            int positivo = 0;
+
+            do
+            {
+                try
+
+                {
+
+                    Console.WriteLine("Introduce un numero");
+                    numero = Int32.Parse(Console.ReadLine());
+
+                    if (numero > 0) 
+                    { 
+                    Console.WriteLine("Numero positivo aceptado");
+                        positivo++;
+                        numeroTotal++;
+                    }
+                    if (numero < 0)
+                    {
+                        Console.WriteLine("Has introducido un numero negativo");
+                        numeroTotal++;
+                    }
+                }
+
+                catch (FormatException)
+
+                {
+
+                    Console.WriteLine("No has introducido un numero");
+
+                }
+
+            } while (numero != 0);
+
+            Console.WriteLine("Has introducido un total de {0} y son positivos {1}", numeroTotal, positivo);
+        }
+    }
+}
+ 
+ 
